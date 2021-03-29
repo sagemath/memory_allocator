0628d8e48e668cae165576b25d70049527a79964
Jonathan Kliem <jonathan.kliem@fu-berlin.de>
2021-03-29 21:27:04 +0200

    remove optional dependency on cysignals

--- memory_allocator/signals_backup.pyx
+++
@@ -1,8 +0,0 @@
-# Backup version, if cysignals is not available.
-# In this case, ``sig_block/sig_unblock`` is not useful anyway.
-
-cdef void sig_block() nogil:
-    pass
-
-cdef void sig_unblock() nogil:
-    pass
